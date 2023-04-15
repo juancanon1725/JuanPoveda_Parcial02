1ea2b78873a645e9660705414072a9f79155ef50
juancanon1725 <98672541+juancanon1725@users.noreply.github.com>
2023-04-15 09:34:02 -0500

    Add files via upload

---
+++ parcial2/parcial2/src/test/java/com/cvds/parcial2/cvds_parcial2/CvdsLab7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvds.lab7.cvds_lab7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvdsLab7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
